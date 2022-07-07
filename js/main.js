389b347104dbe2b4dbc6d51a8a846868a5d9fae3
Skencker <Sandrinekencker@hotmail.com>
2022-07-07 15:32:03 +0200

    text-anim

--- js/main.js
+++ js/main.js
@@ -53,3 +53,37 @@ const handleIntersect = function (entries, observer) {
 }
 const observer = new IntersectionObserver(handleIntersect, options)
 observer.observe(document.querySelector('.svg-circle'))
+
+
+//animation text 
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 100;
+var h6 = document.querySelector('h6');
+// var p = document.querySelector('p');
+var delay = h6.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h6, speed);
+
+
+// type affect to body
+// setTimeout(function(){
+//   p.style.display = "inline-block";
+//   typeEffect(p, speed);
+// }, delay);
